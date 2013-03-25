3059b01c636f1e2e78edbb634dcc036a80fc5600
cappsllc <CAPPSLLC@GMAIL.COM>
2013-03-25 17:19:30 -0400

    Signed-off-by: cappsllc <CAPPSLLC@GMAIL.COM>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'db2e1a330bce16b60670eaef6201f14f0797643d9c895e08ecd89171fde2f995ff10acd1b06d09e3675399cb2b1229c511e40cacbdb41f6ff4bee593db497d81'
